27c825c8fe0f42a6199d9ca6737354aa763dbd5e
jibrilbulama <40578703@live.napier.ac.uk>
2022-04-06 02:33:19 +0100

    database

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const Datastore = require('nedb');
 
--- public/highscore.js
+++ public/highscore.js
@@ -1,36 +1,36 @@
 function setup() {
-    noCanvas();
-    
-   
-    const button = document.getElementById('submit_score');
-    button.addEventListener('click', async event => {
-      const username = document.getElementById('username').value;
-      const userScore= document.getElementById('score_text')
-      
-      const data = { userScore, username };
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      };
-      const response = await fetch('/api', options);
-      const json = await response.json();
-      console.log(json);
+  noCanvas();
+  const video = createCapture(VIDEO);
+  video.size(160, 120);
+  let lat, lon;
+  const button = document.getElementById('submit');
+  button.addEventListener('click', async event => {
+    const mood = document.getElementById('mood').value;
+    video.loadPixels();
+    const image64 = video.canvas.toDataURL();
+    const data = { lat, lon, mood, image64 };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    const response = await fetch('/api', options);
+    const json = await response.json();
+    console.log(json);
+  });
+
+  if ('geolocation' in navigator) {
+    console.log('geolocation available');
+    navigator.geolocation.getCurrentPosition(position => {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      console.log(lat, lon);
+      document.getElementById('latitude').textContent = lat;
+      document.getElementById('longitude').textContent = lon;
     });
-  
-    if ('userScore' in navigator) {
-        document.getElementById("userScore")
-      console.log('userScore available');
-     // navigator.score_text.getCurrentPosition(position => {
-        //lat = position.coords.latitude;
-       // lon = position.coords.longitude;
-       // console.log(score_text);
-        //document.getElementById('latitude').textContent = lat;
-        //document.getElementById('longitude').textContent = lon;
-     // });
-    } else {
-      console.log('userScore not available');
-    }
+  } else {
+    console.log('geolocation not available');
   }
+}
